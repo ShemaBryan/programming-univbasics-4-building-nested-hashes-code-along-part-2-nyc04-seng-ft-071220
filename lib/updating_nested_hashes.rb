de435a2b9b49da087ce97dee8863bde9cf218fc6
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-21 18:46:50 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -20,7 +20,8 @@ def update_status
       }
    }
   epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:capulet][:heroine][:status] = "dead"  # Code your solution here:
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy  # Code your solution here:
 end
 
 def add_characters
